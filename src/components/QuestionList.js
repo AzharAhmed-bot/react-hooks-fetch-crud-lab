faf1b9aaef545e0c85baa763b2d000974d44daad
AzharTakoy <azharahmedtakoy4444@gmail.com>
2023-04-30 17:45:05 +0300

    Complete Assignment

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
-function QuestionList() {
+function QuestionList({ lists, deleteQuestion, fetching }) {
+  function handleClick(answerIndex, questionId) {
+    fetch(`http://localhost:4000/questions/${questionId}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        correctIndex: answerIndex,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        fetching();
+        console.log(data);
+      });
+  }
+
+  const listDisplay =
+    Array.isArray(lists) &&
+    lists.map((list, index) => {
+      return (
+        <>
+          <li key={list.id}>{list.prompt}</li>
+         
+          <h4>Choices:</h4>
+          <li key={list.answers[0]}>(A){list.answers[0]}</li>
+          <li key={list.answers[1]}>(B){list.answers[1]}</li>
+          <li key={list.answers[2]}>(C){list.answers[2]}</li>
+          <li key={list.answers[3]}>(D){list.answers[3]}</li>
+          <button onClick={() => deleteQuestion(list.id)}>Delete Question</button>
+          <label>Correct Answer</label>
+          <select
+            key={index}
+            onChange={(event) => handleClick(event.target.selectedIndex, list.id)}
+            value={list.correctIndex}
+          >
+            <option>{list.answers[0]}</option>
+            <option>{list.answers[1]}</option>
+            <option>{list.answers[2]}</option>
+            <option>{list.answers[3]}</option>
+          </select>
+
+        </>
+      );
+    });
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{listDisplay} </ul>
     </section>
   );
 }
